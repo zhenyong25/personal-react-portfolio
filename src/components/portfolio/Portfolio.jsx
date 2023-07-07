5f1b6fe2c00bd50bd49b527ff2bc5e24109ac7be
zhenyong25 <zhenyong25@gmail.com>
2023-07-07 17:21:02 +0800

    Created all the required components

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './portfolio.css';  
+
+const Portfolio = () => {
+    return <div>About</div>
+}
+
+export default Portfolio;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom"; 
+import App from "./App.jsx"; 
+import "./index.css"; 
 
-function App() {
-  ReactDOM.render(
-    <div className="App">
-    </div>,document.getElementById("root")
-  );
-}
+ ReactDOM.render(
+    <App/>,
+    document.getElementById("root")
+  ) 
 
-export default App;
