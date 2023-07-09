e3bd846c2586f7c484af436670d08f6cd4063afa
zhenyong25 <zhenyong25@gmail.com>
2023-07-10 03:16:40 +0800

    Completed About Me CSS

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,34 +13,34 @@ const About = () => {
             <h5>Get To Know</h5>
             <h2>About Me</h2>
 
-            <div className="container about__container">
+        <div className="container about__container">
                 <div className="about__me">
-                    <div className="about_me-image">
+                    <div className="about__me-image">
                         <img src={ME} alt="About Me"></img>
                     </div>
                 </div>
-            </div>
-
+            
             <div className="about__content">
-
+               
                 <div className="about__cards">
+                    
+                {/* Card 1 */}
                     <article className="about__card">
                         <FaAward className="about__icon"/>
                         <h5>Experience</h5>
                         <small>3+ Years Working</small>
                     </article>
-                </div>
+                
 
-                <div className="about__cards">
+                {/* Card 2 */}
                     <article className="about__card">
                         <FiUsers className="about__icon"/>
                         <h5>Clients</h5>
                         <small>200+ Worldwide</small>
                     </article>
-                </div>
+ 
 
-
-                <div className="about__cards">
+                {/* Card 3 */}
                     <article className="about__card">
                         <VscFolderLibrary className="about__icon"/>
                         <h5>Projects</h5>
@@ -50,11 +50,13 @@ const About = () => {
 
                 {/* Just type "lorem" */}
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis natus ab provident quam, tempora accusamus odio vel, illo reiciendis, optio hic facilis voluptatem! Deserunt vel quasi aliquid sapiente veniam itaque?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis natus ab provident quam, 
+                    tempora accusamus odio vel, illo reiciendis, optio hic facilis voluptatem! Deserunt vel quasi 
+                    aliquid sapiente veniam itaque?
                 </p>
-
                 <a href="#contact" className="btn btn-primary">Let's Talk</a>
             </div>
+        </div>
 
         </section>
     )
