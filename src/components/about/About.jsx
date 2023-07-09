f80948c0c7f62ec1a58c5c074994d303a7dcf054
zhenyong25 <zhenyong25@gmail.com>
2023-07-10 02:34:14 +0800

    Completed About Me JSX

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,61 @@
 import React from "react";
 import './about.css';  
+import ME from "../../assets/profile_pic.png"
+
+// REACT ICONS
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
     return(
         <section id="about">
-            About
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about_me-image">
+                        <img src={ME} alt="About Me"></img>
+                    </div>
+                </div>
+            </div>
+
+            <div className="about__content">
+
+                <div className="about__cards">
+                    <article className="about__card">
+                        <FaAward className="about__icon"/>
+                        <h5>Experience</h5>
+                        <small>3+ Years Working</small>
+                    </article>
+                </div>
+
+                <div className="about__cards">
+                    <article className="about__card">
+                        <FiUsers className="about__icon"/>
+                        <h5>Clients</h5>
+                        <small>200+ Worldwide</small>
+                    </article>
+                </div>
+
+
+                <div className="about__cards">
+                    <article className="about__card">
+                        <VscFolderLibrary className="about__icon"/>
+                        <h5>Projects</h5>
+                        <small>80+ Completed Projects</small>
+                    </article>
+                </div>
+
+                {/* Just type "lorem" */}
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis natus ab provident quam, tempora accusamus odio vel, illo reiciendis, optio hic facilis voluptatem! Deserunt vel quasi aliquid sapiente veniam itaque?
+                </p>
+
+                <a href="#contact" className="btn btn-primary">Let's Talk</a>
+            </div>
+
         </section>
     )
 }
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -12,9 +12,9 @@ import {BsInstagram} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/shoon-zhen-yong-4574a41aa/" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/zhenyong25" target="_blank"><FaGithub/></a>
-        <a href="https://www.instagram.com/zhenyonggg/" target="_blank"><BsInstagram/></a>
+        <a href="https://www.linkedin.com/in/shoon-zhen-yong-4574a41aa/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/zhenyong25" target="_blank" rel="noreferrer"><FaGithub/></a>
+        <a href="https://www.instagram.com/zhenyonggg/" target="_blank" rel="noreferrer"><BsInstagram/></a>
     </div>
   )
 }
