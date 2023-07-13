9b9ae52c960b45c98802ff6de056755ce7e580cf
zhenyong25 <zhenyong25@gmail.com>
2023-07-13 19:04:26 +0800

    Uncomment Testimonials from Profile

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Nav from "./components/nav/Nav"
 import About from "./components/about/About" 
 import Experience from "./components/experience/Experience" 
 import Portfolio from "./components/portfolio/Portfolio" 
-import Testimonials from "./components/testimonials/Testimonials" 
+// import Testimonials from "./components/testimonials/Testimonials" 
 import Contact from "./components/contact/Contact" 
 import Footer from "./components/footer/Footer" 
 
