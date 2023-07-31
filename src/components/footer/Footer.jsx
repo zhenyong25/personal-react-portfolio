8e179de77d8794b371fb4890eab83ec00c8ba477
zhenyong25 <zhenyong25@gmail.com>
2023-08-01 02:43:27 +0800

    Added Internship at Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -16,6 +16,7 @@ const Footer = () => {
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
                 <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#internship">Internship</a></li>
                 {/* <li><a href="#testimonials">Testimonials</a></li> */}
                 <li><a href="#contacts">Contacts</a></li>
             </ul>
